501e525fad364aa395fba13b078c97bee49ac5d1
mazen abdelgowad <mahmedabdelgowad@gmail.com>
2024-07-09 10:33:10 +0300

    solving -> containsDuplicate <- problem

--- problems.cpp
+++ problems.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 using namespace std;
+
+bool containsDuplicate(vector<int> &nums)
+{
+  unordered_set<int> seen{};
+  for (int i = 0; i < nums.size(); i++)
+  {
+    if (seen.count(nums.at(i)) > 0)
+      return true;
+    seen.insert(nums.at(i));
+  }
+
+  return false;
+}
 
 int main()
 {
 
-  cout << "Mazen" << endl;
+  vector<int> nums = {1, 2, 3, 4, 5};
+  cout << containsDuplicate(nums) << endl;
 
   return 0;
 }
